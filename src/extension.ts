165dc3210d3baf32a59317c7b822c32942ea0859
Owen-Hoglund <owenhoglund@gmail.com>
2024-10-06 18:59:09 -0400

    Dropping cursors triggers word-at-cursor selection. Merged dropTail and dropHead - redundant code.

--- src/extension.ts
+++ src/extension.ts
@@ -7,34 +7,54 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "old-hoglie-dynamic-cursor" is now active!');
-	
 	const command1 = vscode.commands.registerCommand('old-hoglie-dynamic-cursor.dropTail', () => {
-		vscode.window.showInformationMessage('Attempting to drop tail of cursors!');
-		const editor = vscode.window.activeTextEditor;
-		if (editor && editor.selections.length > 1) {
-			let selections = [...editor.selections];
-			selections.sort((a, b) => a.active.isAfter(b.active) ? 1:-1 );
-			selections.splice(0, 1);
-			editor.selections = selections;
-		} else {
-			vscode.window.showInformationMessage('Failed to find multiple cursors');
-		}
+		dropCursor('tail');
 	});
 	const command2 = vscode.commands.registerCommand('old-hoglie-dynamic-cursor.dropHead', () => {
-		vscode.window.showInformationMessage('Attempting to drop head of cursors!');
-		const editor = vscode.window.activeTextEditor;
-		if (editor && editor.selections.length > 1) {
-			let selections = [...editor.selections];
-			selections.sort((a, b) => a.active.isAfter(b.active) ? 1:-1 );
-			selections.pop();
-			editor.selections = selections;
-		} else {
-			vscode.window.showInformationMessage('Failed to find multiple cursors');
-		}
+		dropCursor('head');
 	});
 
 	context.subscriptions.push(command1, command2);
 }
 
+
+function dropCursor(dropPosition: string) {
+	const editor = vscode.window.activeTextEditor;
+	if (editor && editor.selections.length > 1) {
+
+		// make a shallow copy of the current selections
+		let selections = [...editor.selections];
+
+		// Sorts selections such that they are ordered top to bottom
+		selections.sort((a, b) => a.active.isAfter(b.active) ? 1:-1 );
+
+		// Drop the specified cursor
+		switch (dropPosition) {
+			case 'tail':
+				selections.splice(0, 1);
+				break;
+			case 'head':
+				selections.pop();
+				break;
+		}
+
+		// Assign the editor selections to the new selections 
+		editor.selections = selections;
+		// this is probably redundant, I think I can probably do the following mapping in situ in the line above, but this definitely works
+		// Maps current selections onto the words at that position such that the user doesn't need to continually reselect
+		editor.selections = [...editor.selections].map(
+			selection => 
+			{
+				const newRange = editor.document.getWordRangeAtPosition(selection.active);
+				if (newRange){
+					return new vscode.Selection(newRange.start, newRange.end);
+				} else {
+					return selection;
+				}
+			}
+		);
+	}
+
+}
 // This method is called when your extension is deactivated
 export function deactivate() {}
